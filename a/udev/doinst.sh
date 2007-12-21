a6de5e65640ef1a6d2a82f05580d5cdcdb609492
Carlos Corbacho <carlos@strangeworlds.co.uk>
2007-12-21 20:03:56 +0000

    a/udev - Upgrade to 116

--- a/udev/doinst.sh
+++ a/udev/doinst.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 config() {
   NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
+
+# Keep same perms on rc.udev.new:
+if [ -e etc/rc.d/rc.udev ]; then
+  cp -a etc/rc.d/rc.udev etc/rc.d/rc.udev.new.incoming
+  cat etc/rc.d/rc.udev.new > etc/rc.d/rc.udev.new.incoming
+  mv etc/rc.d/rc.udev.new.incoming etc/rc.d/rc.udev.new
+fi
+
 config etc/rc.d/rc.udev.new
 config etc/modprobe.d/blacklist.new
 config etc/modprobe.d/isapnp.new
+config etc/modprobe.d/psmouse.new
