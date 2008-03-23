9f96ca08d343451a853aab0cfa436a759c47de51
Fred Emmott <mail@fredemmott.co.uk>
2008-03-23 16:36:56 +0000

    merge in better locale handling for glib2 from slackware

--- l/glib2/glib2.sh
+++ l/glib2/glib2.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 #
-# Description: This script set the environment variables G_FILENAME_ENCODING and  G_BROKEN_FILENAMES 
-#			for Glib library
-#
+# Description:  This script sets the environment variables G_FILENAME_ENCODING
+# and G_BROKEN_FILENAMES for the glib-2.0 library.
 #
 # G_FILENAME_ENCODING
-#       This environment variable can be set to a comma-separated list of character set names. 
-#	GLib assumes that filenames are encoded in the first character set from that list rather than in UTF-8. 
-#	The special token "@locale" can be used to specify the character set for the current locale.
+#       This environment variable can be set to a comma-separated list of
+#       character set names.  GLib assumes that filenames are encoded in the
+#       first character set from that list rather than in UTF-8.  The special
+#       token "@locale" can be used to specify the character set for the
+#       current locale.
+#
+# G_BROKEN_FILENAMES
+#       If this environment variable is set, GLib assumes that filenames are
+#       in the locale encoding rather than in UTF-8.
 
-export G_FILENAME_ENCODING="@locale"
+# If the LANG you have set contains any form of "UTF", we will guess you are
+# using a UTF-8 locale.  Hopefully we're correct.
+if echo $LANG | grep -iq UTF ; then
+  export G_FILENAME_ENCODING="@locale"
+fi
 
-# G_BROKEN_FILENAMES  
-#	If this environment variable is set, GLib assumes that filenames are in the locale encoding rather than in UTF-8. 
-
+# It doesn't hurt to export this since G_FILENAME_ENCODING takes priority
+# over G_BROKEN_FILENAMES:
 export G_BROKEN_FILENAMES=1
 
-# G_FILENAME_ENCODING takes priority over G_BROKEN_FILENAMES. 
