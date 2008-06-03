16440d5700be09892bc11708c65820ad0b984ead
Fred Emmott <mail@fredemmott.co.uk>
2008-06-03 12:47:42 +0100

    updated icecc to 0.9.0

--- extra/icecream/icecc/update-kde-common-admin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-svn export svn://anonsvn.kde.org/home/kde/branches/KDE/3.5/kde-common/admin || exit 1
-rm -f kde-common-admin.tar.bz2
-tar jcfv kde-common-admin.tar.bz2 admin || exit 1
-rm -rf admin
