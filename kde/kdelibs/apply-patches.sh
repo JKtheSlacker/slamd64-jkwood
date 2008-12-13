bd508db8b5ae3379094bb6e1ad4c95d838a6073d
Fred Emmott <mail@fredemmott.co.uk>
2008-12-13 21:56:19 +0000

    removed no-longer-needed kdelibs patches

--- kde/kdelibs/apply-patches.sh
+++ kde/kdelibs/apply-patches.sh
@@ -1,3 +1,1 @@
 zcat $CWD/kdelibs-utempter.diff.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
-zcat $CWD/kdelibs-kate-cursor-fix.diff.gz | patch -p1 --verbose --backup --suffix=.orig || exit 1
-zcat $CWD/post-kde-3.5.5-kinit.diff.gz | patch -p0 --verbose --backup --suffix=.orig || exit 1
